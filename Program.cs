73b639017de82a2324b2430d5c84763083178f4c
Роман <ram23@mail.ru>
2022-04-15 20:47:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static int[] GetArraryFromConsole() 
+        { 
+            int[] result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите число");
+                result[i] = int.Parse(Console.ReadLine());  
+            }
+
+            Console.WriteLine("Массив после сортировки");
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                for (int j = 1+i; j < result.Length; j++)
+                {
+                    if (result[i] > result[j]) 
+                    { 
+                        int temp=result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+
+            return result;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            
+            int[] result=GetArraryFromConsole();
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            
+        }
+    }
+}
